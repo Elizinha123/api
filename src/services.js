1f8a455ce383a223334e2382a288793982d289b4
eliza <elizaxavier2#gmail.com>
2022-05-24 09:32:13 -0300

    alterei

---
+++ src/services.js
@@ -0,0 +1,15 @@
+export function dobro(numero){
+    return numero * 2;
+}
+
+export function somar(a, b){
+    return a + b;
+}
+
+export function media (a, b ,c){
+  return ( a + b + c ) / 3;
+}
+
+export function temperatura (a){
+    return a > 38;
+}
